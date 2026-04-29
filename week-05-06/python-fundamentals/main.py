3bf547c6d5592191d4ebc332daa337bc518abf45
S M Wasir Jayed Rafi <wasir.codes@gmail.com>
2026-04-29 20:01:35 +0600

    W5: utils.py and main.py - module imports

---
+++ week-05-06/python-fundamentals/main.py
@@ -0,0 +1,5 @@
+from utils import c_to_f, add, capitalize_words
+
+print(c_to_f(32))
+print(add(2, 4))
+print(capitalize_words("hello world!"))
---
+++ week-05-06/python-fundamentals/utils.py
@@ -0,0 +1,10 @@
+def c_to_f(temp_c):
+    result = (temp_c * 1.8) + 32
+    return f"{result} Fahrenheit"
+
+def add(a, b):
+    result = a + b
+    return result
+
+def capitalize_words(text):
+    return text.title()
